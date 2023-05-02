461cdb8573e8ce1329442bfe770304487cab1d6d
jamesg6197 <jamesg6197@gmail.com>
2023-05-02 01:00:16 -0700

    Stuff

--- interpreterv1.py
+++ interpreterv1.py
@@ -698,7 +698,7 @@ program_12 = [
       ')',
 
 ]
-interpreter = Interpreter()
+#interpreter = Interpreter()
 # # interpreter.run(program_1) 
 # # print()
 # # interpreter.run(program_2) 
@@ -712,4 +712,4 @@ interpreter = Interpreter()
 # # print()
 # #interpreter.run(program_10)
 # #
-interpreter.run(program_6)
+#interpreter.run(program_6)
